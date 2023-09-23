917f7f5f0f7161e9e8041eb5ad9307accfacd05a
georgedance <133945781+georgedance@users.noreply.github.com>
2023-09-24 01:04:15 +1200

    added various info and memmap printouts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,6 +11,21 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0
 };
 
+static volatile struct limine_bootloader_info_request bootloader_info_request = {
+    .id = LIMINE_BOOTLOADER_INFO_REQUEST,
+    .revision = 0
+};
+
+static volatile struct limine_efi_system_table_request system_table_request = {
+    .id = LIMINE_EFI_SYSTEM_TABLE_REQUEST,
+    .revision = 0
+};
+
+static volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST,
+    .revision = 0
+};
+
 // GCC and Clang reserve the right to generate calls to the following
 // 4 functions even if they are not directly called.
 // Implement them as the C specification mandates.
@@ -95,6 +110,8 @@ static void hcf(void) {
 #define bld "\e[01m"
 #define err_ "[ERROR] "
 #define err clr bld red err_ clr
+#define wrn_ "[WARN] "
+#define wrn clr bld ylw wrn_ clr
 #define inf_ "[INFO] "
 #define inf clr bld blu inf_ clr
 
@@ -105,6 +122,9 @@ static void hcf(void) {
 #define print_error(fs ...) \
     print_term(err, fs)
 
+#define print_warn(fs ...) \
+    print_term(wrn, fs)
+
 #define print_info(fs ...) \
     print_term(inf, fs);
 
@@ -114,6 +134,38 @@ void putchar_(char c) {
     char str[] = {c, '\0'};
     flanterm_write(gft_ctx, str, sizeof(str));
 }
+
+char *memmap_type(uint64_t type) {
+    switch(type) {
+        case 0:
+            return "Usable";
+        case 1:
+            return "Reserved";
+        case 2:
+            return "ACPI Reclaimable";
+        case 3:
+            return "ACPI NVS";
+        case 4:
+            return "BAD MEMORY";
+        case 5:
+            return "Bootloader Reclaimable";
+        case 6:
+            return "Kernel or Modules";
+        case 7:
+            return "Framebuffer";
+        default:
+            return "Unknown";
+    }
+}
+
+#include "../limine-efi/inc/efi.h"
+
+EFI_STATUS              status = EFI_SUCCESS;
+
+EFI_SYSTEM_TABLE        *ST = {0};
+#define gST              ST
+EFI_RUNTIME_SERVICES    *RT = {0};
+#define gRT              RT
 
 // The following will be our kernel's entry point.
 // If renaming _start() to something else, make sure to change the
@@ -138,12 +190,77 @@ void _start(void) {
         framebuffer->address, framebuffer->width, framebuffer->height, framebuffer->pitch
     );
 
-    print_term(bld grn, "%s", "Success! Welcome to the kernel...\n");
+    /*
+    for(int i = 0; i < 128; i++) {
+        printf_("%c", i);
+    }
+    printf_("\n");
+    */
 
-    print_info("framebuffer count: ");
-    printf_("%lu\n", framebuffer_request.response->framebuffer_count);
+    print_info("Initialised FlanTerm.\n");
 
-    print_error("%s", "Missing Kernel Implementation!!!");
+    print_info("Framebuffer Count: %lu\n", framebuffer_request.response->framebuffer_count);
+
+    print_term(bld grn, "Welcome to the kernel...\n");
+
+    if(bootloader_info_request.response == NULL) {
+        print_error("Couldn't get Bootloader Info!\n");
+    }
+    print_info("Bootloader Info: %s, v%s\n",
+            bootloader_info_request.response->name,
+            bootloader_info_request.response->version);
+
+    if(system_table_request.response == NULL
+    || system_table_request.response->address == NULL) {
+        print_error("Couldn't get SystemTable Pointer!\n");
+        hcf();
+    }
+    else {
+        print_info("Acquired SystemTable Pointer.\n");
+        gST = system_table_request.response->address;
+        gRT = gST->RuntimeServices;
+    }
+
+    print_info("Firmware Vendor: %ls, Revision %d\n", gST->FirmwareVendor, gST->FirmwareRevision);
+
+    EFI_TIME Time;
+    gRT->GetTime(&Time, NULL);
+    print_info("Time: %s%hhu:%hhu:%hhu\n", 
+            (Time.TimeZone == 2047 ? "(UTC) " : "" ), 
+            Time.Hour, Time.Minute, Time.Second);
+
+    if(memmap_request.response == NULL) {
+        print_error("Couldn't get memmap!\n");
+    }
+    else {
+        print_info("Memmap entry count: %lu\n", memmap_request.response->entry_count);
+        struct limine_memmap_response *memmap_response = memmap_request.response;
+        struct limine_memmap_entry *memmap = *memmap_response->entries;
+        for(size_t i = 0; i < memmap_response->entry_count; i++) {
+            printf_("count: %-3lu base: 0x%-10lX length: 0x%-10lX type: %s\n", 
+                        i, memmap->base, memmap->length, memmap_type(memmap->type));
+            memmap++;
+        }
+    }
+
+
+    print_error("Missing Kernel Implementation!!!\n");
+
+    //print_warn("System Shutting down now");
+    print_warn("System halting now");
+    for(int i = 0; i < 5; i++) {
+        EFI_TIME Timer;
+        gRT->GetTime(&Timer, NULL);
+        UINT8 secs = Timer.Second;
+        UINT8 secs_old = secs;
+        while(secs == secs_old) {
+            gRT->GetTime(&Timer, NULL);
+            secs = Timer.Second;
+        }
+        printf_(".");
+    }
+
+    //gRT->ResetSystem(EfiResetShutdown, status, 0, NULL);
 
     // We're done, just hang...
     hcf();
--- kernel/printf/printf.c
+++ kernel/printf/printf.c
@@ -78,11 +78,13 @@
 #endif
 
 // Support for the decimal notation floating point conversion specifiers (%f, %F)
+// default is 1
 #ifndef PRINTF_SUPPORT_DECIMAL_SPECIFIERS
 #define PRINTF_SUPPORT_DECIMAL_SPECIFIERS 0
 #endif
 
 // Support for the exponential notation floating point conversion specifiers (%e, %g, %E, %G)
+// default is 1
 #ifndef PRINTF_SUPPORT_EXPONENTIAL_SPECIFIERS
 #define PRINTF_SUPPORT_EXPONENTIAL_SPECIFIERS 0
 #endif
