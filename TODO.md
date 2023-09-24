fdf8c435651a9ee38938f15eaba781a9f1180429
georgedance <133945781+georgedance@users.noreply.github.com>
2023-09-25 03:32:43 +1300

    bump TODO.md, remove line drawing

--- TODO.md
+++ TODO.md
@@ -3,6 +3,11 @@ TODO
 
 - [X] load GDT??? (Global Descriptor Table) - Done for us by Limine Bootloader
 - [ ] load IDT (Interrupt Descriptor Table)
-- [ ] get uefi boot services handle
+- [ ] load and parse memmap
 - [ ] write simple shell, only builtins
+- [ ] write keyboard driver
+- [ ] write own video driver
+- [ ] write filesystem driver
+
+- [X] get uefi ~~boot~~ runtime services handle
 
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -181,10 +181,12 @@ void _start(void) {
     struct limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
     // Note: we assume the framebuffer model is RGB with 32-bit pixels.
+    /*
     for (size_t i = 0; i < 100; i++) {
         volatile uint32_t *fb_ptr = framebuffer->address;
         fb_ptr[i * (framebuffer->pitch / 4) + i] = 0xffffff;
     }
+    */
 
     gft_ctx = flanterm_fb_simple_init(
         framebuffer->address, framebuffer->width, framebuffer->height, framebuffer->pitch
